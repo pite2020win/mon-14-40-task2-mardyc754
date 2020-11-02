e4e1782a53a16f35cbe28648ecb40ea96bf63c4c
mardyc754 <57150292+mardyc754@users.noreply.github.com>
2020-11-02 16:14:18 +0100

    Update task.py

--- task.py
+++ task.py
@@ -4,7 +4,7 @@ def tilt_correction():
   while True:
     rate_of_correction = random.randint(10,100)/10
     tilt = random.randint(-10,10)
-    print(f"The plane rolled {tilt} degrees")
+    print(f"Current orientation: {tilt} degrees")
     print("Tilt correction:")  
     time.sleep(0.5)
     tilt -= random.gauss(0, 2*rate_of_correction)
