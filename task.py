d1f244b428b67b2886eeec19279f51d9aed298d8
mardyc754 <57150292+mardyc754@users.noreply.github.com>
2020-11-02 16:12:10 +0100

    Update task.py

--- task.py
+++ task.py
@@ -1,24 +1,14 @@
-#
-#Flight simulator. 
-#Write a code in python that simulates the tilt correction of the plane (angle between plane wings and earth). 
-#The program should print out current orientation, and applied tilt correction.
-# (Tilt is "Roll" in this diagram https://www.novatel.com/assets/Web-Phase-2-2012/Solution-Pages/AttitudePlane.png)
-#The program should run in infinite loop, until user breaks the loop. 
-#Assume that plane orientation in every new simulation step is changing with random angle with gaussian distribution (the planes is experiencing "turbuence"). 
-# Hint: "random.gauss(0, 2*rate_of_correction)"
-#With every simulation step the orentation should be corrected, correction should be applied and printed out.
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#Delete these comments before commit!
-#
-#Good luck.
+import random, time
+
+def tilt_correction():
+  while True:
+    rate_of_correction = random.randint(10,100)/10
+    tilt = random.randint(-10,10)
+    print(f"The plane rolled {tilt} degrees")
+    print("Tilt correction:")  
+    time.sleep(0.5)
+    tilt -= random.gauss(0, 2*rate_of_correction)
+    print(f"Plane tilt: {tilt} degrees")
+    time.sleep(1)
+
+tilt_correction()
